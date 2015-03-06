57c871c3ba674a0099c19adf622d57b5e6924df5
Aleksa <rakicaleksa@ymail.com>
2015-03-06 23:23:04 +0100

    C finished

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ with: %r{\.(gif|jpg|png)\Z}i,
 message: 'must be a URL for GIF, JPG or PNG image.'
 }
 validates :title, length: { minimum: 2 }
+
+def self.latest
+    Product.order(:updated_at).last
+  end
 end
